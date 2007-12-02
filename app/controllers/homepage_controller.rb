fbf5ec6c7ad5ad52735d29291d0f49e04000031a
Patric Mueller <bhaak@gmx.net>
2007-12-02 12:20:33 +0000

    Verwende in_production?

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -6,7 +6,7 @@ class HomepageController < ApplicationController
     minTime = Time.rfc2822(request.env["HTTP_IF_MODIFIED_SINCE"]) rescue nil
     @texte = Onlinetextelink.find :all, :order => "onlinetextelinks.created_at desc",:limit => 15, :include => [:onlinetext, {:onlinetext => :autoren}]
     if minTime and @texte[0].created_at <= minTime then
-      render :nothing => true, :status => 304
+      render :nothing => true, :status => 304 if in_production?
     else
       headers["Content-Type"] = "application/xml"
       response.headers['Last-Modified'] = @texte[0].created_at.httpdate
