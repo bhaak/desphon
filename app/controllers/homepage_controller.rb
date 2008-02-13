2f12b99e01fa69e7d7db60572a3dff0620265be7
Patric Mueller <bhaak@gmx.net>
2008-02-13 23:34:36 +0000

    RSS: 100 Stueck pro Abruf und MIME-Typ in Titel

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -10,7 +10,7 @@ class HomepageController < ApplicationController
 		if minTime and neueste <= minTime then
 			render :nothing => true, :status => 304
 		else
-			@texte = Onlinetextelink.find :all, :order => "onlinetextelinks.created_at desc",:limit => 15, :include => [:onlinetext, {:onlinetext => :autoren}]
+			@texte = Onlinetextelink.find :all, :order => "onlinetextelinks.created_at desc",:limit => 100, :include => [:onlinetext, {:onlinetext => :autoren}]
 			@news = Comatose::Page.find_by_slug('news').children.reverse.slice(0..4) # die fünf letzten Nachrichten
 			headers["Content-Type"] = "application/xml"
 			render :layout => false 
